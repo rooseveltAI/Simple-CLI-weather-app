980a069d410f01ba2d89c387143259be0dd900ab
Roosevelt <howdy.roosevelt@gmail.com>
2019-12-07 12:04:04 +0100

    Weather cli

--- lib/weather_cli.dart
+++ lib/weather_cli.dart
@@ -1,3 +1,29 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:cli/runes.dart';
+import 'package:cli/api.dart';
+
+Future weatherCli(String zip, String country) async {
+  if (zip.isEmpty || country.isEmpty) {
+    stderr.writeln('error: arguments missing');
+  } else {
+    try {
+      final apiResult = await getWather(zip: zip, country: country);
+      stdout.writeln('');
+      stdout.writeln(
+          'Weather in ${apiResult.name} ${String.fromCharCodes(rainBow)}');
+      stdout.writeln('---------------------------------------');
+
+      final date =
+          DateTime.fromMillisecondsSinceEpoch(apiResult.dt.toInt() * 1000);
+      final dateTimeString = '${date.day}.${date.month}.${date.year} :';
+      final tempString =
+          'Now: ${apiResult.main.temp} °C, Min: ${apiResult.main.tempMin} °C, Max: ${apiResult.main.tempMax} °C';
+      stdout.writeln(dateTimeString);
+      stdout.writeln(tempString);
+      stdout.writeln('');
+    } catch (e) {
+      stderr.writeln('error: networking error');
+      stderr.writeln(e.toString());
+    }
+  }
 }
